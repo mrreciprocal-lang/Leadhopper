3af7ccbc2ffd8ed4a12938d8616b47916d5f4019
mrreciprocal-lang <mr.reciprocal@gmail.com>
2026-09-12 18:05:02 -0400

    Bump Lead Hopper to 1.7.4 for locked monoglyph build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.kyle.leadhopper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.7.3"
+        versionCode = 23
+        versionName = "1.7.4"
     }
 
     buildTypes {
