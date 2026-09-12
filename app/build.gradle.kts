7126274c9eaa267a9f1462fcc644d09eb9c353c7
mrreciprocal-lang <mr.reciprocal@gmail.com>
2026-09-12 02:48:47 -0400

    Add Lead Hopper V14 adaptive launcher icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.kyle.leadhopper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.3.0"
+        versionCode = 14
+        versionName = "1.4.0"
     }
 
     buildTypes {
