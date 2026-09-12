75f7183ac987d976ccfb3f5531431a89b5c90635
mrreciprocal-lang <mr.reciprocal@gmail.com>
2026-09-12 17:08:30 -0400

    Bump Lead Hopper to 1.7.3 for redesigned launcher icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.kyle.leadhopper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.7.2"
+        versionCode = 22
+        versionName = "1.7.3"
     }
 
     buildTypes {
