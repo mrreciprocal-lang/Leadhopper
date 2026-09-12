50bb494c885bf86974fc72a309c64741031334af
mrreciprocal-lang <mr.reciprocal@gmail.com>
2026-09-12 18:24:35 -0400

    Bump Lead Hopper to 1.7.5 build 24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.kyle.leadhopper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.7.4"
+        versionCode = 24
+        versionName = "1.7.5"
     }
 
     buildTypes {
