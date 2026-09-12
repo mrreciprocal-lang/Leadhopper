794280b4e6371c7766dfc1be3e4e9739203822c0
mrreciprocal-lang <mr.reciprocal@gmail.com>
2026-09-12 10:29:23 -0400

    Keep Lead Hopper on 1.7.0 while incrementing internal build code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.kyle.leadhopper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
+        versionCode = 18
         versionName = "1.7.0"
     }
 
