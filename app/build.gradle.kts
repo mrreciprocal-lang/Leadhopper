889db388fc447e0c7eaa24d6d05ae652564d3418
mrreciprocal-lang <mr.reciprocal@gmail.com>
2026-09-13 17:49:43 -0400

    Bump bridge candidate build number for safe in-place upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.kyle.leadhopper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 26
+        versionCode = 100
         versionName = "1.7.7-bridge"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
