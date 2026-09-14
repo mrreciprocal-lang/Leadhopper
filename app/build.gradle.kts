1bdc17afe1ab8dbe2a0d3377d17917d983bc939e
mrreciprocal-lang <mr.reciprocal@gmail.com>
2026-09-13 20:48:33 -0400

    Start lightweight 1.7.5 cleanline build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.kyle.leadhopper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "1.7.5"
+        versionCode = 102
+        versionName = "1.7.5-cleanline"
     }
 
     buildTypes {
